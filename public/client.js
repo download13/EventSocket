3c5406a218364587a7f5e4cdbb7ca9bd59dbc00e
download13 <download333@gmail.com>
2012-01-25 11:27:30 -0800

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,43 @@
+// EventEmitter over WebSocket stream
+(function(global) {
+	var slice = Array.prototype.slice;
+	var UNPACK = JSON.parse;
+	
+	function EventSocket(socket) {
+		socket.onmessage = this._onmessage.bind(this);
+		this._socket = socket;
+		this._handlers = {};
+	}
+	EventSocket.prototype = {
+		_onmessage: function(message) {
+			message = UNPACK(message);
+			this.emit.apply(message);
+		},
+		
+		on: function(type, listener) {
+			if(!this._handlers[type]) this._handlers[type] = [];
+			
+			this._handlers[type].push(listener);
+		},
+		emit: function(type) {
+			if(!this._handlers[type]) this._handlers[type] = [];
+			
+			var args = slice.call(arguments, 1);
+			var listeners = this._handlers[type];
+			if(listeners == null) return;
+			for(var i = 0; i < listeners.length; i++) {
+				listeners[i].apply(this, args);
+			}
+		},
+		removeListener: function(type, listener) {
+			if(this._handlers[type] == null) return;
+			
+			var listeners = this._handlers[type];
+			var pos = listeners.indexOf(listener);
+			if(pos == -1) return;
+			delete this._handlers[type][pos];
+		}
+	};
+	
+	global.EventSocket = EventSocket;
+})(this);
